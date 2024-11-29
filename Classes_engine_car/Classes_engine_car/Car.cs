e36719c31288d653bf86e3226abfdf5145c167da
Lesya Danko <lesyadankoit@gmail.com>
2024-11-29 08:35:22 -0500

    added Car, Engine classes

---
+++ Classes_engine_car/Classes_engine_car/Car.cs
@@ -0,0 +1,37 @@
+﻿namespace Classes_engine_car
+{
+    public class Car
+    {
+        private Engine _engine;
+
+        public Car(Engine engine)
+        {
+            if (engine != null)
+            {
+                _engine = engine;
+            }
+            else
+            {
+                _engine = new Engine();
+            }
+        }
+
+        public bool FillTank(int fuel, out string reason)
+        {
+            if (_engine == null)
+            {
+                reason = "Engine is null";
+                return false;
+            }
+
+            if (_engine.TrySetFuel(fuel))
+            {
+                reason = "All ok";
+                return true;
+            }
+
+            reason = "fuel is not valid";
+            return false;
+        }
+    }
+}
---
+++ Classes_engine_car/Classes_engine_car/Engine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_engine_car
+{
+    public class Engine
+    {
+        private int _minFuel;
+        private int _maxFuel;
+
+        private int _fuel;
+
+        public Engine()
+        {
+            _fuel = 0;
+            _minFuel = 0;
+            _maxFuel = 100;
+        }
+
+        public bool TrySetFuel(int fuel)
+        {
+            if (fuel > _maxFuel || fuel < _minFuel)
+            {
+                return false;
+            }
+
+            _fuel = fuel;
+            return true;
+        }
+
+        public int GetFuel()
+        {
+            return _fuel;
+        }
+    }
+}
